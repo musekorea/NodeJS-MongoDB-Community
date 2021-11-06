74c607d440cf3fae65dd5e977c4c3d27b9144e7b
musekorea <musekorea@hanmail.net>
2021-11-06 17:22:12 +0800

    build2

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import regeneratorRuntime from 'regenerator-runtime';
 import { connectDB } from './db.js';
 import app from './server.js';
 
-app.listen(process.env.SERVER_PORT, async () => {
+app.listen(process.env.PORT, async () => {
   await connectDB();
-  console.log(`Server is listening on Port 8080`);
+  console.log(`Server is listening on Port ${process.env.PORT}`);
 });
